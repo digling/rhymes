b5ba5c2100509007d545cf38d9da739fc90a4ca2
LinguList <mattis.list@uni-marburg.de>
2016-12-04 13:19:51 +0100

    updated data

--- datasets/Wang1980/raw/parse.py
+++ datasets/Wang1980/raw/parse.py
@@ -1,12 +1,14 @@
 import re
 from sinopy import sinopy
 
-infile = open('wang-1980-raw.txt')
+infile = open('wang-1980-raw-2.txt')
 poem, stanza = 0, 0
 warnings = 0
 def aline(line, warnings):
 
     comment = ''
+    problem = False
+    problems = ['']
     rhymewords = []
     reconstruction = ''
     previous = ''
@@ -15,6 +17,11 @@ def aline(line, warnings):
     for char in line:
         if char == ' ':
             pass
+        elif char == '<':
+            problem = True
+        elif char == '>':
+            problem = False
+            problems += ['']
         elif char == '[':
             inbrackets = True
         elif inbrackets and char != ']':
@@ -34,12 +41,14 @@ def aline(line, warnings):
         elif incomment:
             comment += char
         else:
-            if sinopy.is_chinese(char):
+            if problem:
+                problems[-1] += char
+            elif sinopy.is_chinese(char):
                 previous = char
                 stripped_line += char
             elif char in '?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz':
                 rhymewords += [[previous, char]]
-    return stripped_line, rhymewords, comment, warnings
+    return stripped_line, rhymewords, comment, warnings, ','.join(problems)
 
 D = {}
 idx = 1
@@ -60,23 +69,24 @@ for i, line in enumerate(infile):
         if ecount <= 1 and poem:
             lines = ln.split('、')
             for i, l in enumerate(lines):
-                s, r, c, warnings = aline(l, warnings)
+                s, r, c, warnings, problems = aline(l, warnings)
                 print(s, len(r), c, warnings)
                 if r:
                     for recs in r:
                         if len(recs) != 3:
                             recs = recs + ['???', '???', '???']
                         D[idx] = [poem, name, stanza, i, l, s, recs[0], recs[1],
-                                recs[2], c]
+                                recs[2], c, problems]
                         idx += 1
                 else:
-                    D[idx] = [poem, name, stanza, i, l, s, '', '', '', c]
+                    D[idx] = [poem, name, stanza, i, l, s, '', '', '', c,
+                            problems]
                     idx += 1
     else:
         ecount += 1
         stanza += 1
 
 with open('Wang1980.tsv', 'w') as f:
-    f.write('ID\tPOEM_NUMBER\tPOEM_NAME\tSTANZA\tLINE_IN_SOURCE\tLINE\tRHYME\tRHYMEWORD\tRECONSTRUCTION\tCOMMENT\n')
+    f.write('ID\tPOEM_NUMBER\tPOEM_NAME\tSTANZA\tLINE_IN_SOURCE\tLINE\tRHYME\tRHYMEWORD\tRECONSTRUCTION\tCOMMENT\tCATEGORY\tPROBLEMS\n')
     for k in sorted(D):
         f.write(str(k)+'\t'+'\t'.join([str(x) for x in D[k]])+'\n')
